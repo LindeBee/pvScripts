c46922866e7d8d9bb32cbad51275ae20da3106fa
LindeBee <38701378+LindeBee@users.noreply.github.com>
2021-06-04 18:26:18 +0200

    change legend location

--- inflowValidation.py
+++ inflowValidation.py
@@ -12,8 +12,7 @@ paraview.simple._DisableFirstRenderCameraReset()
 # Create a new 'Line Chart View'
 lineChartView1 = CreateView('XYChartView')
 lineChartView1.ViewSize = [459, 422]
-lineChartView1.LegendLocation = 'Custom'
-lineChartView1.LegendPosition = [44, 529]
+lineChartView1.LegendLocation = 'TopLeft'
 lineChartView1.LeftAxisUseCustomRange = 1
 lineChartView1.LeftAxisRangeMinimum = 0.
 lineChartView1.LeftAxisRangeMaximum = 3.
@@ -27,7 +26,7 @@ lineChartView1.BottomAxisTitle = '<u/u_H>'
 # Create a new 'Line Chart View'
 lineChartView2 = CreateView('XYChartView')
 lineChartView2.ViewSize = [459, 422]
-lineChartView2.LegendPosition = [770, 791]
+lineChartView2.LegendLocation = 'TopRight'
 lineChartView2.LeftAxisRangeMaximum = 3.0
 lineChartView2.BottomAxisRangeMaximum = 0.04
 lineChartView2.LeftAxisTitle = 'z/H'
@@ -44,7 +43,7 @@ layout1 = CreateLayout(name='Layout #1')
 layout1.SplitHorizontal(0, 0.500000)
 layout1.AssignView(1, lineChartView1)
 layout1.AssignView(2, lineChartView2)
-layout1.SetSize(919, 845)
+layout1.SetSize(459, 845)
 
 # ----------------------------------------------------------------
 # restore active view
